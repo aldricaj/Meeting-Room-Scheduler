83223617192221f34a58ddcdbf4595c98b68d1a0
lawrencj <lawrencj@mail.uc.edu>
2018-07-24 15:30:10 -0400

    -times fixed

--- room-scheduler/static/index.js
+++ room-scheduler/static/index.js
@@ -128,14 +128,14 @@ if (!loadFromLocal('ran')) {
     schedule: [
       {
         meeting_id: 1,
-        meeting_start: new Date(2018, 6, 25, 9, 0),
-        meeting_end: new Date(2018, 6, 25, 10, 0),
+        meeting_start: "10:00",
+        meeting_end: "12:00",
         meeting_title: "Post Mortem"
       },
       {
         meeting_id: 2,
-        meeting_start: new Date(2018, 6, 26, 13,0),
-        meeting_end: new Date(2018, 6, 26, 14,0),
+        meeting_start: "12:00",
+        meeting_end: "1:30",
         meeting_title: "Sprint Planning"
       }
     ]
@@ -151,14 +151,14 @@ if (!loadFromLocal('ran')) {
     schedule: [
       {
         meeting_id: 3,
-        meeting_start: new Date(2018, 6, 24, 14, 0),
-        meeting_end: new Date(2018, 6, 24, 16, 0),
+        meeting_start: "1:00",
+        meeting_end: "4:00",
         meeting_title: "Long Meeting"
       },
       {
         meeting_id: 4,
-        meeting_start: new Date(2018, 6, 26, 13,0),
-        meeting_end: new Date(2018, 6, 26, 14,0),
+        meeting_start: "3:00",
+        meeting_end: "4:00",
         meeting_title: "Short Meetings"
       }
     ]
@@ -174,14 +174,14 @@ if (!loadFromLocal('ran')) {
     schedule: [
       {
         meeting_id: 6,
-        meeting_start: new Date(2018, 6, 24, 12, 0),
-        meeting_end: new Date(2018, 6, 24, 16, 0),
+        meeting_start: "12:20",
+        meeting_end: "2:30",
         meeting_title: "Ultra Long Meeting"
       },
       {
         meeting_id: 7,
-        meeting_start: new Date(2018, 6, 27, 13,0),
-        meeting_end: new Date(2018, 6, 27, 14,0),
+        meeting_start: "2:00",
+        meeting_end: "2:30",
         meeting_title: "Short Meetings"
       }
     ]
@@ -192,7 +192,12 @@ $(document).ready(function(){
   
   var meetingRooms = getRooms();
   meetingRooms.forEach(function(meetingRoom){
-    $(".allRooms").append("<div class='box effect8'><div class='col-sm-12 room'><div class='room-number col-sm-6'><h2>Room "+meetingRoom.roomNumber+"</h2></div><div class='col-sm-6'><a id='newMeeting' style='float: right; margin-top: 24px;'><h4>+ Add New Meeting </h4></a></div></div><hr><div class='col-sm-6'>{{Meeting.meetingName}}</div><div class='col-sm-6'>{{Meeting.startTime}} - {{Meeting.endTime}}</div> <div class='col-sm-10'><table align='center' style='width:100%'><tr><th></th><th>8am</th> <th>9am</th><th>10am</th><th>11am</th><th>12pm</th><th>1pm</th><th>2pm</th><th>3pm</th><th>4pm</th><th>5pm</th><th>6pm</th><th>7pm</th></tr><tr><th>Mon</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Tues</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Wed</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Thurs</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Fri</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Sat</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Sun</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr></table></div><div class='col-sm-2' style='position: absolute;bottom: 0;right: -50px;'><p>Media Availability:</p> <div class='circle green'></div><br /><p>AV Type: Projector</p></div></div><script>var modal = document.getElementById('myModal');	var btn = document.getElementById('newMeeting');var span = document.getElementsByClassName('close')[0];var submit = document.getElementsByClassName('btn')[0];btn.onclick = function() {modal.style.display = 'block';}span.onclick = function() {modal.style.display = 'none';}submit.onclick = function() {modal.style.display = 'none'}window.onclick = function(event) {if (event.target == modal) {modal.style.display = 'none';}}</script>")
+    var meetings = [];
+    var schedule = meetingRoom.schedule
+    schedule.forEach(function(meeting){
+      meetings.push(meeting);
+    })
+    $(".allRooms").append("<div class='box effect8'><div class='col-sm-12 room'><div class='room-number col-sm-6'><h2>Room "+meetingRoom.roomNumber+"</h2></div><div class='col-sm-6'><a id='newMeeting' style='float: right; margin-top: 24px;'><h4>+ Add New Meeting </h4></a></div></div><hr><div class='col-sm-6'>"+meetings[0].meeting_title+"</div><div class='col-sm-6'>"+meetings[0].meeting_start+" - "+meetings[0].meeting_end+"</div> <div class='col-sm-10'><table align='center' style='width:100%'><tr><th></th><th>8am</th> <th>9am</th><th>10am</th><th>11am</th><th>12pm</th><th>1pm</th><th>2pm</th><th>3pm</th><th>4pm</th><th>5pm</th><th>6pm</th><th>7pm</th></tr><tr><th>Mon</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Tues</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Wed</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Thurs</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Fri</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Sat</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><th>Sun</th><td></td> <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr></table></div><div class='col-sm-2' style='position: absolute;bottom: 0;right: -50px;'><p>Media Availability:</p> <div class='circle green'></div><br /><p>AV Type: Projector</p></div></div><script>var modal = document.getElementById('myModal');	var btn = document.getElementById('newMeeting');var span = document.getElementsByClassName('close')[0];var submit = document.getElementsByClassName('btn')[0];btn.onclick = function() {modal.style.display = 'block';}span.onclick = function() {modal.style.display = 'none';}submit.onclick = function() {modal.style.display = 'none'}window.onclick = function(event) {if (event.target == modal) {modal.style.display = 'none';}}</script>")
   });
   if(window.location.href === "http://localhost:5080/"){
     $("[href='/']").hide()
